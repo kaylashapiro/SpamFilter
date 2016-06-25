9f94a6b8da9ed902a3a844c901e6a18003b93205
kaylashapiro <kshapi17@gmail.com>
2016-06-25 16:55:52 +0100

    Main reads in BinaryFeatures.csv and Labels.csv. Finished cost function. TODO: gradient, regLogisticRegression

--- Code/logisticReg.py
+++ Code/logisticReg.py
@@ -1,65 +1,78 @@
 
 # coding: utf-8
 
-# In[18]:
-
 import numpy as np
+import pandas as pd
 
-
-# In[19]:
-
+# Definition of the sigmoid function
 def sigmoid(z):
     prediction = 1.0/(1.0 + np.exp(-z))
     
     return prediction
     
-
-
-# In[20]:
-
+# Hypothesis function
 def hypothesis(theta, x):
     z = np.dot(theta,x)
     prediction = sigmoid(z)
     
     return prediction
 
-
-# In[21]:
+	
 
 # Cost for a given training example
-
 def costFunction(prediction, trueValue):
     cost = trueValue * np.log(prediction) + (1 - trueValue) * np.log(1 - prediction)
     
+    print 'Prediction:', prediction, 'Class:', trueValue
+    print 'Cost of prediction:', cost
+	
     return cost
 
 
-# In[50]:
-
-# Total cost over n training instances
-
-def totalCost(predictions, trueValues):
+# Total cost over n training instances; This is the function we are minimizing
+def computeCost(predictions, trueValues):
     n_instances = len(predictions)
     cost_sum = 0
     
     for instance in range(0, n_instances):
-        cost_sum += costFunction(predictions[instance], trueValues[instance])
-        print cost_sum
+        cost_sum -= costFunction(predictions[instance], trueValues[instance])
+    
+    
+    print 'Sum cost is', cost_sum
+    
+    cost = (1.0/n_instances)*cost_sum
+    print 'Cost is ', cost
+    
+    return cost
+    
 
+    
+# Cost for a given training example
+def gradient(x, trueValue, theta, j, m):
+    grad = (hypothesis(theta,x) - trueValue) * x[j]
+    print 'TODO'
 
-# In[60]:
 
 # INPUT: X := 2D 
-
 def regLogisticRegression():
     print 'TODO'
     
 
-
-# In[61]:
-
 # Main function to write tests
 def main():
+
+    df_X = pd.read_csv('BinaryFeatures.csv', header = None)
+    print df_X
+    
+    X = np.array(df_X) 
+    print X
+    
+    df_y = pd.read_csv('Labels.csv', header = None)
+    print df_y
+    
+    y = np.array(df_y)
+    print y
+
     regLogisticRegression()
     
     prediction = sigmoid(2)
@@ -68,10 +81,8 @@ def main():
     cost = costFunction(prediction, 0)
     print cost
     
-    totalCost([.98,.002,.89],[1,0,1])
+    computeCost([.98,.002,.89],[1,0,1])
 
-
-# In[62]:
 
 # Standard boilerplate to call the main() function to begin
 # the program.
