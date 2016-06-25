f3f5a9c96f8067a36c5a13826d923b31f07c8b48
kaylashapiro <kshapi17@gmail.com>
2016-06-25 18:10:03 +0100

    Logistic Regression functions are implemented. TODO: test, implement scoring threshold, compare to other logistic regression implementations, think about termination conditions

--- Code/logisticReg.py
+++ Code/logisticReg.py
@@ -30,8 +30,7 @@ def costFunction(prediction, trueValue):
 
 
 # Total cost over n training instances; This is the function we are minimizing
-def computeCost(predictions, trueValues):
-    n_instances = len(predictions)
+def computeCost(predictions, trueValues, n_instances):
     cost_sum = 0
     
     for instance in range(0, n_instances):
@@ -47,31 +46,64 @@ def computeCost(predictions, trueValues):
     
 
     
-# Cost for a given training example
-def gradient(x, trueValue, theta, j, m):
-    grad = (hypothesis(theta,x) - trueValue) * x[j]
-    print 'TODO'
+# Gradient for a given feature
+def costGradient(X, y, theta, j, n_instances): 
+    grad_sum = 0
+    
+    for i in range(0, n_instances)
+        x_i = X[i]
+        x_ij = X[j]
+        
+        y_i = y[0][i]
+        
+        h = hypothesis(theta, x_i)
+        
+        grad_sum += (h - y_i) * x_ij
+        
+    grad = (1.0/float(n_instances)) * grad_sum
+    
+    print 'The gradient is', grad
+    return grad
+    
 
+# Gradient descent
+def gradientDescent(X, y, theta, n_instances, n_features, alpha):
+    new_theta = []
+    
+    for j in range(n_features):
+        new_thetaj = theta[j] - alpha * costGradient(X,y,theta,j,n_instances)
+        new_theta.append(new_thetaj)
+        
+    return new_theta
 
 # INPUT: X := 2D 
-def regLogisticRegression():
-    print 'TODO'
+def regLogisticRegression(X, y, theta, alpha, n_iters):
+    n_instances, n_features = X.shape
+    
+    for x in range(0,n_iters):
+        theta = gradientDescent(X,y,theta,n_instances,n_features,alpha)
+    
+    return theta
+    
     
 
 # Main function to write tests
 def main():
 
     df_X = pd.read_csv('BinaryFeatures.csv', header = None)
-    print df_X
+    #print df_X
     
     X = np.array(df_X) 
     print X
     
     df_y = pd.read_csv('Labels.csv', header = None)
-    print df_y
+    #print df_y
     
     y = np.array(df_y)
     print y
+    
+    #n_instances, n_features = X.shape
+    #print n_instances, n_features
 
     regLogisticRegression()
     
@@ -81,7 +113,8 @@ def main():
     cost = costFunction(prediction, 0)
     print cost
     
-    computeCost([.98,.002,.89],[1,0,1])
+    computeCost([.98,.002,.89],[1,0,1], 2)
+    
 
 
 # Standard boilerplate to call the main() function to begin
@@ -89,8 +122,6 @@ def main():
 if __name__ == '__main__':
   main()
 
-
-# In[ ]:
 
 
 
