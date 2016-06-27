a79fe7b3a09569bdd77c3d440944e89558bc0db8
kaylashapiro <kshapi17@gmail.com>
2016-06-27 14:58:45 +0100

    Generates bootstrap replicate datasets using sampling with replacement. Currently prints the results. Will feed into bagging program (TODO); Need to also incorporate class values, but that is quick

--- Code/sampleWithReplacement.py
+++ Code/sampleWithReplacement.py
@@ -11,7 +11,7 @@ import sys
 def sampleWithReplacement(n_instances, new_instances):
     return np.random.choice(n_instances, new_instances, replace=True)
     
-# Generate a single bootstrap replicate set    
+# Generate a single bootstrap replicate set.    
 def generateReplicate(X, new_instances):
     n_instances = X.shape[0]
     
@@ -24,29 +24,34 @@ def generateReplicate(X, new_instances):
         replicate = np.concatenate((replicate, sample), axis=0)
      
     return replicate
-        
-        
     
 
-# Main function to create bootstrap replicate sets
-# argv[1] := number of replicates to generate
+# Function to generate a certain number of replicate sets.
+# Currently just prints the replicates. But, will export to .csv files 
+# or feed directly into bagging algorithm
+def generateBootstraps(X, new_instances, n_replicates):
+    for i in range(0, n_replicates):
+        replicate = generateReplicate(X, new_instances)
+        print replicate
+        
+        
+# Main function to create bootstrap replicate sets.
+# argv[1] := number of replicates to generate.
 def main():
-  if len(sys.argv) >= 2:
-	n_replicates = sys.argv[1]
-  else:
-    n_replicates = 25
+    if len(sys.argv) >= 2:
+        n_replicates = int(sys.argv[1])
+    else:
+        n_replicates = 25
   
+    new_instances = 5
   
-  n_instances = 5
-  new_instances = 5
+    # Let's get a dataset up in this bizniz
+    df_X = pd.read_csv('test.csv', header = None)
+    X = np.array(df_X)
+    #print X
   
-  # Let's get a dataset up in this bizniz
-  df_X = pd.read_csv('test.csv', header = None)
-  X = np.array(df_X)
-  #print X
-  
-  print generateReplicate(X, new_instances)
+    generateBootstraps(X, new_instances, n_replicates)
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
-  main()
+    main()
