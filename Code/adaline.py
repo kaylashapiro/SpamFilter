a4f0e921916cc0b8a16510fff19b39be4a14494e
kaylashapiro <kshapi17@gmail.com>
2016-08-01 00:36:48 +0100

    Fixed Bug; Note: cost explodes for dictionary attack alpha > .01

--- Code/adaline.py
+++ Code/adaline.py
@@ -2,16 +2,9 @@
 
 '''
 Adapted from: https://github.com/galvanic/adversarialML/blob/master/adaline.py
+
 Implementation of the Adaline model.
 Training is done using batch gradient descent.
-
-TODO ? make an Adaline class with train and test as methods
-TODO ? implement regularisation
-TODO ? cost and error could be measured outside the function
-     or at least use a callable to calculate them, otherwise duplicated code
-     across models
-TODO clean up the code further, especially duplicated sections (adaline model
-     etc.)
 '''
 import sys
 import numpy as np
@@ -24,8 +17,7 @@ def addBias(X):
     '''
     Adds bias term to the training set.
     
-    Input:
-    
+    Input:  
     - X: N * D Numpy matrix of binary feature values (0 and 1)
          with N: the number of training examples
          and  D: the number of features for each example
@@ -34,7 +26,6 @@ def addBias(X):
     - X_bias: N * (D + 1) Numpy matrix of binary feature values
               consisting of a column of ones + X
     '''
-    #print X.shape
     X_bias = np.insert(X, 0, 1, axis=1)
     
     return X_bias
@@ -42,8 +33,16 @@ def addBias(X):
 
 def computeCost(trueValues, predictions):
     '''
-    Calculate cost using Means Squared
-    '''
+    Calculate cost using means squared
+    
+    Input:
+    - trueValues: N * 1 Numpy vector of binary labels
+                  with N: the number of examples
+    - predictions: N * 1 Numpy vector of predicted values
+    
+    Output:
+    - cost: real number calculated using means squared
+    '''  
     trueValues, predictions = map(np.ravel, [trueValues, predictions]) ## make sure shape is (len,) for both
     cost = np.mean(np.square(trueValues - predictions))
     
@@ -53,18 +52,19 @@ def computeCost(trueValues, predictions):
 def fit(features, labels,
         ## params:
         initial_weights=None,
-        learning_rate=0.01,
-        termination_condition=max_iters(100),
+        learning_rate=0.05,
+        termination_condition=None,
+        threshold=1e-5,
         ham_label=0,
         spam_label=1,
-        verbose=False,
+        verbose=True,
+        add_bias = True
         ):
     '''
     Returns the optimal weights for a given training set (features
     and corresponding label inputs) for the ADALINE model.
     These weights are found using the gradient descent method.
         
-    TRAINING PHASE
     Inputs:
     - features: N * D Numpy matrix of binary values (0 and 1)
         with N: the number of training examples
@@ -73,15 +73,17 @@ def fit(features, labels,
     - initial_weights: D * 1 Numpy vector, beginning weights
     - learning_rate: learning rate, a float between 0 and 1
     - termination_condition: returns a bool
+    
     Output:
     - W: D * 1 Numpy vector of real values
-    TODO yield cost, error, weights as it is learning ?
-         this could allow possibility to inject new learning rate during
-         training
-    TODO implement an autostop if cost is rising instead of falling ?
-    '''
+    '''   
+    if not termination_condition:
+        termination_condition = max_iters(50)
+        
+    if (add_bias):
+        features = addBias(features)
+    
     ## 0. Prepare notations
-    features = addBias(features)
     X, Y = features, labels
     N, D = features.shape   # N #training samples; D #features
     cost = []               # keep track of cost
@@ -91,9 +93,9 @@ def fit(features, labels,
     W = np.zeros((D, 1)) if initial_weights is None else initial_weights.reshape((D, 1))
 
     ## 2. Evaluate the termination condition
-    i = 1
+    epoch = 1
+    last_epoch_error = 1e6
     while not termination_condition():
-
         ## current iteration classifier output
         O = np.dot(X, W)
 
@@ -101,10 +103,9 @@ def fit(features, labels,
         ## _before_ the activation function
         ## batch gradient descent
         gradient = -np.mean(np.multiply((Y - O), X), axis=0)
-        gradient = gradient.reshape(W.shape)
 
         ## 3. Update weights
-        W = W - learning_rate * gradient
+        W = W - learning_rate * gradient.reshape(W.shape)
 
         ## Keep track of error and cost (weights from previous iteration)
         ## T is equivalent to threshold/step activation function
@@ -117,31 +118,45 @@ def fit(features, labels,
 
         current_error = computeError(T, Y)
         error.append(current_error)
-        current_cost = computeCost(Y, O)
-        cost.append(current_cost)
+        
+        #current_cost = computeCost(Y, O)
+        #cost.append(current_cost)
+        
+        # THINK ABOUT THE EPOCH VALUE
+        if (epoch > 100 and np.abs(last_epoch_error - current_error) < threshold):
+            break
+            
+        last_epoch_error = current_error
 
-        if verbose: print('iteration %d:\tcost = %.3f' % (i, cost[-1]))
-        i += 1
+        if verbose and (epoch%10 == 0): print('iteration %d:\tcost = %.3f' % (epoch, cost[-1]))
+        epoch += 1
 
-    return W#, cost, error, current_error
+    return W
 
 
 def predict(features, weights,
         ## params
         ham_label=0,
         spam_label=1,
+        add_bias = True
         ):
     '''
-    TEST PHASE
-    TODO not sure what makes sense to measure here ?
-         => performance can be calculated outside the function
+    Input:
+    - features: N * D Numpy matrix of binary values (0 and 1)
+        with N: the number of training examples
+        and  D: the number of features for each example
+    - weights: D * 1 Numpy vector of real values
+    
+    Output:
+    - T: N * 1 Numpy vector of binary prediction values
     '''
-    features = addBias(features)
+    if (add_bias):
+        features = addBias(features)
     
     ## notation
     X, W = features, weights
     N, D = features.shape
-    #print W
+    
     ## apply model
     O = np.dot(X, W)
 
@@ -153,83 +168,34 @@ def predict(features, weights,
     else:   ## ham label is assumed -1, spam label assumed 1
         T = np.ones(O.shape)
         T[O < 0] = -1
-    #print T
+    
     return T
 
 
 def main():
     '''Test Adaline training'''
-    ## dummy data
-    ## 10 training samples and 3 features
-    ## so a 10 * 3 matrix 
-    '''
-    x = np.array([[1, 0, 1],
-                  [0, 0, 0],
-                  [1, 0, 1],
-                  [1, 1, 1],
-                  [1, 1, 0],
-                  [1, 1, 0],
-                  [1, 1, 0],
-                  [1, 1, 0],
-                  [1, 1, 0],
-                  [0, 1, 0]],
-                  dtype=np.int8)
-    y = np.array([[1],
-                  [1],
-                  [1],
-                  [0],
-                  [0],
-                  [0],
-                  [0],
-                  [0],
-                  [0],
-                  [1]],
-                  dtype=np.int8) #* 2 - 1
-    '''
+
     df_x = pd.read_csv('../Datasets/EmptyAttackData/10_perc_poison/X_train_0.csv', header = None)
     x = np.array(df_x)
-    #x = addBias(x)
-    print x
     
     df_y = pd.read_csv('../Datasets/EmptyAttackData/10_perc_poison/y_train_0.csv', header = None)
-    y = np.array(df_y)
-    print y   
+    y = np.array(df_y)   
    
-    df_x_test = pd.read_csv('../Datasets/EmptyAttackData/10_perc_poison/X_test_0.csv', header = None)
-    x = np.array(df_x)
-    #x = addBias(x)
-    #print x
+    df_x_test = pd.read_csv('../Datasets/TestData/X_test_0.csv', header = None)
+    x_test = np.array(df_x_test)
     
-    df_y_test = pd.read_csv('../Datasets/EmptyAttackData/10_perc_poison/y_test_0.csv', header = None)
-    y = np.array(df_y)
-    #print y   
+    df_y_test = pd.read_csv('../Datasets/TestData/y_test_0.csv', header = None)
+    y_test = np.array(df_y_test)
    
     ## train model
-    weights, cost, error, final_error = fit(features=x, labels=y,
-        learning_rate=.3,
-        termination_condition=max_iters(100))
-    #print('weights: %s' % ([' %.3f' % w for w in weights]))
-    #print cost
-    #print error
+    weights = fit(features=x, labels=y,
+        learning_rate=.2,
+        termination_condition=max_iters(1000))
+        
+
     predictions = predict(x_test, weights)
-    #print predictions
-    print 'Final Error:', final_error
     
-    plt.figure(figsize=(10,6), dpi=120)
-        
-    plt.plot(error, color='blue', label='Error')
-    plt.plot(cost, color='red', label = 'Cost')
-    
-    #plt.title('')
-    plt.xlabel('Iterations')
-    plt.ylabel('Rate')
-    
-    #plt.xlim(0, no_predictors + 1)
-    #plt.ylim(0)
-        
-    plt.legend(loc='upper right', frameon=True)
-    
-    plt.show()
+    print computeError(y_test, predictions)
     
     return
 
