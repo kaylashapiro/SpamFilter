f6a081959daf7cad2b8420e07673caf2e580bc67
kaylashapiro <kshapi17@gmail.com>
2016-08-09 18:39:45 +0100

    Adaline with a dirty adaptive learning rate implementation

---
+++ Code/boldAdaline.py
@@ -0,0 +1,201 @@
+# coding: utf-8
+
+'''
+Adapted from: https://github.com/galvanic/adversarialML/blob/master/adaline.py
+
+Implementation of the Adaline model.
+Training is done using batch gradient descent.
+'''
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from metrics import computeError
+from helpers import addBias
+from gradientDescent import max_iters  
+
+def computeCost(trueValues, predictions):
+    '''
+    Calculate cost using means squared
+    
+    Input:
+    - trueValues: N * 1 Numpy vector of binary labels
+                  with N: the number of examples
+    - predictions: N * 1 Numpy vector of predicted values
+    
+    Output:
+    - cost: real number calculated using means squared
+    '''  
+    trueValues, predictions = map(np.ravel, [trueValues, predictions]) ## make sure shape is (len,) for both
+    cost = np.mean(np.square(trueValues - predictions))
+    
+    return cost
+    
+
+def fit(features, labels,
+        ## params:
+        initial_weights=None,
+        learning_rate=0.1,
+        max_epoch = 200,
+        threshold=1e-5,
+        ham_label=0,
+        spam_label=1,
+        verbose=True,
+        add_bias = True
+        ):
+    '''
+    Returns the optimal weights for a given training set (features
+    and corresponding label inputs) for the ADALINE model.
+    These weights are found using the gradient descent method.
+        
+    Inputs:
+    - features: N * D Numpy matrix of binary values (0 and 1)
+        with N: the number of training examples
+        and  D: the number of features for each example
+    - labels:   N * 1 Numpy vector of binary values (-1 and 1)
+    - initial_weights: D * 1 Numpy vector, beginning weights
+    - learning_rate: learning rate, a float between 0 and 1
+    - termination_condition: returns a bool
+    
+    Output:
+    - W: D * 1 Numpy vector of real values
+    '''           
+    if (add_bias):
+        features = addBias(features)
+    
+    ## 0. Prepare notations
+    X, Y = features, labels
+    N, D = features.shape   # N #training samples; D #features
+    cost = []               # keep track of cost
+    error = []              # keep track of error
+
+    ## 1. Initialise weights
+    W = np.zeros((D, 1)) if initial_weights is None else initial_weights.reshape((D, 1))
+
+    ## 2. Evaluate the termination condition
+    epoch = 0
+    last_epoch_error = 1e10
+
+    while epoch < max_epoch:
+        ## current iteration classifier output
+        
+        last_W = W
+        
+        O = np.dot(X, W)
+
+        ## specialty of ADALINE is that training is done on the weighted sum,
+        ## _before_ the activation function
+        ## batch gradient descent
+        gradient = -np.mean(np.multiply((Y - O), X), axis=0)
+
+        ## 3. Update weights
+        W = W - learning_rate * gradient.reshape(W.shape)
+
+        ## Keep track of error and cost (weights from previous iteration)
+        ## T is equivalent to threshold/step activation function
+        if ham_label is 0:               ## spam label assumed 1
+            T = np.zeros(O.shape)
+            T[O > 0.5] = 1
+        else:   ## ham label is assumed -1, spam label assumed 1
+            T = np.ones(O.shape)
+            T[O < 0] = -1
+
+        current_error = computeError(T, Y)
+        error.append(current_error)
+        
+        current_cost = computeCost(Y, O)
+        cost.append(current_cost)
+        
+        if (current_error < last_epoch_error): 
+            current_error = computeError(T, Y)
+            error.append(current_error)
+            learning_rate = learning_rate*1.05
+            print 'LEARNING RATE INCREASED TO:', learning_rate
+        elif (current_error - last_epoch_error > 1e-8):
+            learning_rate = learning_rate*.5
+            W = last_W
+            epoch -= 1
+            current_error = 1e10
+            print 'LEARNING RATE DECREASED TO:', learning_rate
+            print 'UNDO ITERATION!!!!!!'
+        else:
+            current_error = computeError(T, Y)
+            error.append(current_error)
+            
+        #if (epoch > 50 and np.abs(last_epoch_error - current_error) < threshold):
+        #    break
+            
+        epoch += 1
+            
+        last_epoch_error = current_error
+
+        if verbose and (epoch%1 == 0): print('iteration %d:\tcost = %.3f \terror = %.3f' % (epoch, cost[-1], error[-1]))
+       
+    return W
+
+
+def predict(features, weights,
+        ## params
+        ham_label=0,
+        spam_label=1,
+        add_bias = True
+        ):
+    '''
+    Input:
+    - features: N * D Numpy matrix of binary values (0 and 1)
+        with N: the number of training examples
+        and  D: the number of features for each example
+    - weights: D * 1 Numpy vector of real values
+    
+    Output:
+    - T: N * 1 Numpy vector of binary prediction values
+    '''
+    if (add_bias):
+        features = addBias(features)
+    
+    ## notation
+    X, W = features, weights
+    N, D = features.shape
+    
+    ## apply model
+    O = np.dot(X, W)
+
+    ## calculate output
+    ## T is equivalent to threshold/step activation function
+    if ham_label is 0:               ## spam label assumed 1
+        T = np.zeros(O.shape)
+        T[O > 0.5] = 1
+    else:   ## ham label is assumed -1, spam label assumed 1
+        T = np.ones(O.shape)
+        T[O < 0] = -1
+    
+    return T
+
+
+def main():
+    '''Test Adaline training'''
+
+    df_x = pd.read_csv('../Datasets/DictAttackData/30_perc_poison/X_train_0.csv', header = None)
+    x = np.array(df_x)
+    
+    df_y = pd.read_csv('../Datasets/DictAttackData/30_perc_poison/y_train_0.csv', header = None)
+    y = np.array(df_y)   
+   
+    df_x_test = pd.read_csv('../Datasets/TestData/X_test_0.csv', header = None)
+    x_test = np.array(df_x_test)
+    
+    df_y_test = pd.read_csv('../Datasets/TestData/y_test_0.csv', header = None)
+    y_test = np.array(df_y_test)
+   
+    ## train model
+    weights = fit(features=x, labels=y)
+        
+
+    predictions = predict(x_test, weights)
+    
+    print computeError(y_test, predictions)
+    
+    return
+
+if __name__ == '__main__':
+    sys.exit(main())
