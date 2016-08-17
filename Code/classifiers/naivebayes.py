1581d586771ee8b6e39d1b2ca555b94decda1bff
kaylashapiro <kshapi17@gmail.com>
2016-08-17 16:34:18 +0100

    Got rid of main function

--- Code/classifiers/naivebayes.py
+++ Code/classifiers/naivebayes.py
@@ -113,30 +113,3 @@ def predict(features, parameters,
         predicted = predicted * 2 - 1
 
     return np.array(predicted).astype(int) #To match the format of my other classifiers and for bagging
-
-def main():
-    '''Test NaiveBayes training'''
-
-    df_x = pd.read_csv('../../Datasets/TrainData/lingspam/X_train_1.csv', header = None)
-    x = np.array(df_x)
-    
-    df_y = pd.read_csv('../../Datasets/TrainData/lingspam/y_train_1.csv', header = None)
-    y = np.array(df_y)
-   
-    df_x_test = pd.read_csv('../../Datasets/TestData/lingspam/X_test_1.csv', header = None)
-    x_test = np.array(df_x_test)
-    
-    df_y_test = pd.read_csv('../../Datasets/TestData/lingspam/y_test_1.csv', header = None)
-    y_test = np.array(df_y_test)
-   
-    ## train model
-    weights = fit(x, y)        
-
-    predictions = predict(x_test, weights)
-    
-    print computeError(y_test, predictions)
-    
-    return
-
-if __name__ == '__main__':
-    main()
