ae362952f2c761e29fe0685935ce839ab3cc7043
kaylashapiro <kshapi17@gmail.com>
2016-08-18 16:14:49 +0100

    Can now plot bagging vs base with and without poisoning

--- Code/bagging_plot.py
+++ Code/bagging_plot.py
@@ -38,52 +38,21 @@ def bag_vs_base_plot(classifier, dataset, attack, percent_poisoning):
     
     N = attack_bag.shape[0]
     
-    error_plot = bag_vs_base_plot_error(no_attack_base[0][0], no_attack_bag[:,0], attack_base[0][0], attack_bag[:,0], N)
-
-def bag_vs_base_plot_error(classifier, dataset, attack, percent_poisoning):
-    no_attack_path = get_results_path(classifier, dataset, 'No')
-    attack_path = get_results_path(classifier, dataset, attack, percent_poisoning)
+    error_plot = bag_vs_base_plot_error(classifier, attack, percent_poisoning,
+                                        no_attack_base[0][0], no_attack_bag[:,0], attack_base[0][0], attack_bag[:,0], N)
+    error_plot.show()
     
-    base_file = '0_0_0.csv'
-    bagging_file = '100_100_0.csv'
-    
-    no_attack_base = no_attack_path + base_file
-    no_attack_bag = no_attack_path + bagging_file
-    attack_base = attack_path + base_file
-    attack_bag = attack_path + bagging_file
-    
-    df_no_attack_base = pd.read_csv(no_attack_base, header = 0)
-    no_attack_base = np.array(df_no_attack_base)
-    no_attack_base_error = no_attack_base[0][0]
-    
-    df_no_attack_bag = pd.read_csv(no_attack_bag, header = 0)
-    no_attack_bag = np.array(df_no_attack_bag)
-    no_attack_bag_errors = no_attack_bag[:,0]
-    
-    df_attack_base = pd.read_csv(attack_base, header = 0)
-    attack_base = np.array(df_attack_base)
-    #print attack_base
-    attack_base_error = attack_base[0][0]
-    #print attack_base_error
-    
-    df_attack_bag = pd.read_csv(attack_bag, header = 0)
-    attack_bag = np.array(df_attack_bag)
-    #print attack_bag
-    attack_bag_errors = attack_bag[:,0]
-    #print attack_bag_error
-    
-    N = attack_bag.shape[0]
-    
+def bag_vs_base_plot_error(classifier, attack, percent_poisoning,
+                           no_attack_base_error, 
+                           no_attack_bag_errors, 
+                           attack_base_error,
+                           attack_bag_errors,
+                           N,
+                           ):    
     no_attack_base_errors = np.repeat(no_attack_base_error, N)
     attack_base_errors = np.repeat(attack_base_error, N)
     
-    print no_attack_base_errors
-    print no_attack_bag_errors
-    print attack_base_errors
-    print attack_bag_errors
-    
     X = np.linspace(1, N, num=N, endpoint=True)
-    print X
     
     title = get_attack_name(attack, percent_poisoning)
     
@@ -102,7 +71,8 @@ def bag_vs_base_plot_error(classifier, dataset, attack, percent_poisoning):
                           label='Bagged (poisoned)')
     
     legend = plt.legend(loc='upper right', shadow=True, prop={'size':12})
-    plt.show()
+    
+    return plt
     
 def get_attack_name(attack, percent_poisoning):
     attacks = {
@@ -152,7 +122,7 @@ def main():
     attack = 'Dict'
     percent_poisoning = 10
     
-    bag_vs_base_plot_error(classifier, dataset, attack, percent_poisoning)
+    bag_vs_base_plot(classifier, dataset, attack, percent_poisoning)
 
 if __name__ == '__main__':
     main()
