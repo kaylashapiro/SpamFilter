8443252940e04bfa1aabd20283c941d8fcbec472
kaylashapiro <kshapi17@gmail.com>
2016-08-23 15:35:43 +0100

    naivebayes, Dict, 10

--- Code/run_tests.py
+++ Code/run_tests.py
@@ -359,15 +359,15 @@ def main():
     dataset='enron'
     
     ## SELECT ATTACK ('No', 'Dict', 'Empty', 'Ham', 'Ham2')
-    attack='Ham'
+    attack='Dict'
     
     ## SELECT CLASSIFIER ('logistic_regression', 'adaline', 'naivebayes')
-    classifier = 'boldAdaline'
+    classifier = 'naivebayes'
     
     # SELECT PERCENT OF POISONING
     #perc_poisoning = [0] # No Attack
     #perc_poisoning = [10, 20, 30] # Attack
-    perc_poisoning = [30]
+    perc_poisoning = [10]
     
     # BAGGING PARAMETERS
     bagging_samples = [.6, .8, 1.0]
