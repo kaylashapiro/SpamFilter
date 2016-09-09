0d3817eaa3f1fb72abd987f3a29ab76201e856b6
kaylashapiro <kshapi17@gmail.com>
2016-09-09 10:47:03 +0100

    Cleaned up AdaBoost

--- Code/ensembles/adaboost.py
+++ Code/ensembles/adaboost.py
@@ -2,18 +2,13 @@
 
 import sys
 import numpy as np
-import pandas as pd
-from sklearn.linear_model import SGDClassifier as weak_learner
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier as weak_learner
 
-sys.path.insert(0, '../classifiers')
 sys.path.insert(0, '../helpers')
 
 import metrics as met
-from add_bias import addBias
-from featureSubsampling import featureSubsampling
 
-def boost(X, y, sample_weight, learning_rate=.1, epochs=100, terminate=False, classifier='DecisionTree'):
+def boost(X, y, sample_weight, learning_rate=.1, epochs=100, epsilon=1e-10):
     '''
     One iteration of the Adaboost algorithm to build a classifier.
     
@@ -26,17 +21,16 @@ def boost(X, y, sample_weight, learning_rate=.1, epochs=100, terminate=False, cl
     - sample_weight: 1 * N Numpy vector of sample weightings
     - learning_rate: real number step size
     - epochs: number of iterations
-    - terminate: boolean termination condition
+    - epsilon: small term to avoid divide by zero error
     
     Output:
     - classifier: trained classifier object
     - new_sample_weight: 1 * N Numpy vector of sample weightings
     - alpha: weight of the classifier prediction
-    - terminate: boolean termination condition
     '''
     N = X.shape[0]
 
-    classifier = weak_learner(loss='log', penalty='none')
+    classifier = weak_learner(max_depth=1)
     
     # sklearn implementation requires y to be flattened
     y = np.ravel(y)
@@ -46,31 +40,21 @@ def boost(X, y, sample_weight, learning_rate=.1, epochs=100, terminate=False, cl
     classifier.fit(X, y, sample_weight=sample_weight)
     
     predictions = classifier.predict(X)
-    print 'Predictions:', predictions    
     
     indicator = (predictions != y).astype(int)
-    print 'Indicator:', indicator
     
     # 2. Measure the error based on the probability distribution
     error = np.dot(sample_weight, indicator)
     
-    # 3. If error == 0, we fit the data perfectly
-    if error == 0:
-        terminate = True
-        alpha = float('inf')
-        return classifier, sample_weight, alpha, terminate
+    # 3. Determine the weight of classifier
+    alpha = .5 * np.log((1-error + epsilon)/error + epsilon)
     
-    # 4. Determine the weight of classifier
-    alpha = .5 * np.log((1-error)/error)
-    print 'e=%.5f a=%.5f'%(error, alpha)
-    
-    # 5. Update the distribution
+    # 4. Update the distribution
     new_sample_weight = sample_weight * np.exp(-alpha*indicator)
-    print 'New weights:', new_sample_weight
     
-    return classifier, new_sample_weight, alpha, terminate
+    return classifier, new_sample_weight, alpha
 
-def fit(X, y, X_test, y_test, 
+def fit(X, y, 
         learning_rate=.1, 
         epochs=100, 
         no_predictors=50, 
@@ -90,6 +74,8 @@ def fit(X, y, X_test, y_test,
     - epochs: number of iterations
     - no_predictors: number of iterations of boosting
     
+    /!\ Assumes bias term is already in the X input.
+    
     Output:
     - classifiers: list of classifier objects at each boosting 
                    iteration
@@ -98,39 +84,16 @@ def fit(X, y, X_test, y_test,
     N = X.shape[0]
     classifiers = []
     alphas = []
-    errors = []
-    TPRs = []
-    FPRs = []
-    FNRs = []
-    TNRs = []
-    AUCs = []
     
     # 0. Initialize uniform weights
     sample_weight = np.array([float(1)/N,] * N)
-    print 'Initial Weights:', sample_weight
     
     for iter in xrange(no_predictors):
-        classifier, sample_weight, alpha, terminate = boost(X, y, sample_weight)
+        classifier, sample_weight, alpha = boost(X, y, sample_weight)
         classifiers.append(classifier)
         alphas.append(alpha)
-        
-        predictions = predict(X_test, classifiers, alphas)
-        
-        errors.append(met.computeError(y_test, predictions))
-        
-        AUCs.append(met.computeAUC(y_test, predictions))
-                
-        [TPR, FPR, FNR, TNR] = met.computeRates(y_test, predictions, ham_label, spam_label)
-        
-        TPRs.append(TPR)
-        FPRs.append(FPR)
-        FNRs.append(FNR)
-        TNRs.append(TNR)
-                
-        if terminate:
-            return (classifiers, alphas, errors, TPRs, FPRs, FNRs, TNRs, AUCs)
-    
-    return (classifiers, alphas, errors, TPRs, FPRs, FNRs, TNRs, AUCs)
+                           
+    return (classifiers, alphas)
     
 def predict(X, classifiers, alphas):
     '''
@@ -143,6 +106,8 @@ def predict(X, classifiers, alphas):
     - classifiers: list of classifier objects at each boosting 
                    iteration
     - alphas: weights of each classifier's prediction
+    
+    /!\ Assumes bias term is already in the X input.
     
     Output:
     - y_predictions: 1 * N Numpy vector of binary values (0 and 1);
@@ -160,54 +125,4 @@ def predict(X, classifiers, alphas):
     y_predictions[y_predictions==-1] = 0
     
     return y_predictions
-    
-    
-    
-def main():
-
-    
-    df_x = pd.read_csv('../../Datasets/TrainData/enron/X_train_0.csv', header = None)
-    x = np.array(df_x)
-    print x
-    
-    x_train, x_test = featureSubsampling(x, x, .7)
-    x_train = addBias(x_train)
-    x_test = addBias(x_test)
-    
-    df_y = pd.read_csv('../../Datasets/TrainData/enron/y_train_0.csv', header = None)
-    y = np.ravel(np.array(df_y))
-    print y   
-    
-    # BASE CLASSIFIER
-    classifier = weak_learner(loss='log', penalty='none')
-    classifier.fit(x, y)
-    predictions = classifier.predict(x)
-    print 'BASE CLASSIFIER ERROR:', met.computeError(y, predictions)
-    
-    
-    classifiers, alphas, errors, TPRs, FPRs, FNRs, TNRs, AUCs = fit(x,y,x,y,no_predictors=10)
-    print 'Final Classifier:', classifiers[-1] 
-    
-    predictions = predict(x, classifiers, alphas)
-    print 'Final Predictions:', predictions
-    
-    print 'Errors:', errors
-    print 'TPRs:', TPRs
-    print 'FPRs:', FPRs
-    print 'FNRs:', FNRs
-    print 'TNRs:', TNRs
-    
-    error = met.computeError(y, predictions)
-    print 'Final Error:', error
-    
-    classifier = AdaBoostClassifier(n_estimators=50, learning_rate=.1)
-    classifier.fit(x,np.ravel(y))
-    pred = classifier.predict(x)
-    print pred
-    err = met.computeError(y, pred)
-    print 'SKLEARN ERROR:', err
-    
-    return
-
-if __name__ == '__main__':
-    main()
+   
